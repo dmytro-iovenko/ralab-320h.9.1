559d51c5eb643cddf04979a5f7642bb91bc52e75
Dmytro Iovenko <iovenko@gmail.com>
2024-09-29 00:25:23 -0400

    add [type=edit] logic

--- src/components/TaskItem.jsx
+++ src/components/TaskItem.jsx
@@ -1,3 +1,22 @@
+function EditTask({ task, dispatch }) {
+  return (
+    <>
+      <input type="text" value={task.title} />
+      <button>Save</button>
+    </>
+  );
+}
+
+function SaveTask({ task, dispatch }) {
+  return (
+    <>
+      <label htmlFor={task.id}>{task.title}</label>
+      <button onClick={() => dispatch({ type: "edit", payload: { id: task.id } })}>Edit</button>
+      <button onClick={() => dispatch({ type: "delete", payload: { id: task.id } })}>Delete</button>
+    </>
+  );
+}
+
 export default function TaskItem({ task, dispatch }) {
   return (
     <li>
@@ -7,9 +26,7 @@ export default function TaskItem({ task, dispatch }) {
         checked={task.completed}
         onChange={() => dispatch({ type: "complete", payload: { id: task.id, completed: !task.completed } })}
       />
-      <label htmlFor={task.id}>{task.title}</label>
-      <button>Edit</button>
-      <button onClick={() => dispatch({ type: "delete", payload: { id: task.id } })}>Delete</button>
+      {task.edit ? <EditTask task={task} dispatch={dispatch} /> : <SaveTask task={task} dispatch={dispatch} />}
     </li>
   );
 }
--- src/utils/todoReducer.js
+++ src/utils/todoReducer.js
@@ -8,6 +8,11 @@ export default function reducer(state, { type, payload }) {
       });
     case "delete":
       return state.filter((task) => task.id !== payload.id);
+    case "edit":
+      return state.map((task) => {
+        task.id === payload.id && (task.edit = true);
+        return task;
+      });
   }
   return state;
 }
