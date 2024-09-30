83f40eb85f49068503356f0770066ef5cc6df786
Dmytro Iovenko <iovenko@gmail.com>
2024-09-30 18:06:57 -0400

    calculate request time

--- src/data/todos-api.js
+++ src/data/todos-api.js
@@ -2,12 +2,39 @@ import axios from "axios";
 
 axios.defaults.baseURL = "https://jsonplaceholder.typicode.com/";
 
+axios.interceptors.request.use(
+  (config) => {
+    config.metadata = { ...config.metadata, elapsedTime: {} };
+    config.metadata.elapsedTime.start = Date.now();
+    console.log("Axious request begins:", config);
+    return config;
+  },
+  (error) => {
+    console.log("Axious request failed:", error);
+    return error;
+  }
+);
+
+axios.interceptors.response.use(
+  (response) => {
+    const elapsedTime = response.config.metadata.elapsedTime;
+    elapsedTime.end = Date.now();
+    elapsedTime.total = elapsedTime.end - elapsedTime.start;
+    console.log(`The request took ${elapsedTime.total} ms.`);
+    return response;
+  },
+  (error) => {
+    console.log("Axious response failed:", error);
+    return error;
+  }
+);
+
 export async function getTodos() {
-    const response = await axios.get("todos");
-    return response.data;
+  const response = await axios.get("todos");
+  return response.data;
 }
 
 export async function createTodoTask(taskBody) {
-    const response = await axios.post("todos", taskBody);
-    return response.data;
+  const response = await axios.post("todos", taskBody);
+  return response.data;
 }
